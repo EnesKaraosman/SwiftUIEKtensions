cc900d40132d85cd2e6899ae0fdfcf87e9e12eef
EXT02D19224 <enes.karaosman@consultant.turkcell.com.tr>
2020-06-30 10:07:11 +0300

    [ADD] AppStorage propertyWrapper added (this functionality supported built in for iOS 14+)

---
+++ Sources/SwiftUIEKtensions/Other/AppStorage.swift
@@ -0,0 +1,53 @@
+//
+//  AppStorage.swift
+//  
+//
+//  Created by Enes Karaosman on 30.06.2020.
+//
+
+import Foundation
+
+/*
+ Usage:
+ 
+ struct AppData {
+     @Apptorage("user_name", defaultValue: "")
+     static var userName: String
+ }
+ 
+ AppData.userName // ""
+ AppData.userName = "Enes"
+ AppData.userName // "Enes"
+
+ **/
+
+@available(iOS, introduced: 14.0, unavailable)
+@propertyWrapper
+struct AppStorage<T: Codable> {
+    
+    let key: String
+    let defaultValue: T
+    let store: UserDefaults
+    
+    init(wrappedValue: T, _ key: String, store: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = wrappedValue
+        self.store = store
+    }
+    
+    var wrappedValue: T {
+        get {
+            guard let data = store.object(forKey: key) as? Data else {
+                return defaultValue
+            }
+            let value = try? JSONDecoder().decode(T.self, from: data)
+            return value ?? defaultValue
+        }
+        set {
+            let data = try? JSONEncoder().encode(newValue)
+            store.set(data, forKey: key)
+            store.synchronize()
+        }
+    }
+    
+}
